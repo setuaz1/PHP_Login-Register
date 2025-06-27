2b43cee269d7083de54571ae7d13c24c8c7b0d77
a.tsurkan <paparap021@gmail.com>
2025-06-27 14:08:36 +0300

    final

--- login_register.php
+++ login_register.php
@@ -18,31 +18,30 @@ if (isset($_POST['Register'])) {
     } else {
         $conn->query("INSERT INTO users (name, email, password, role) VALUES ('$name', '$email', '$password', '$role')");
     }
-
     header("Location: index.php");
     exit();
+}
 
-    if (isset($_POST['Login'])) {
-        $email = $_POST['email'];
-        $password = $_POST['password'];
 
-        $result = $conn->query("SELECT * FROM users WHERE email = '$email'");
-        if ($result->num_rows > 0) {
-            $user = $result->fetch_assoc();
-            if (password_verify($password, $user['password'])) {
-                $_SESSION['name'] = $user['name'];
-                $_SESSION['email'] = $user['email'];
+if (isset($_POST['Login'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-                if ($user['role'] === 'admin') {
-                    header("Location: admin_page.php");
-                } else {
-                    header("Location: user_page.php");
-                }
-                exit();
+    $result = $conn->query("SELECT * FROM users WHERE email = '$email'");
+    if ($result->num_rows > 0) {
+        $user = $result->fetch_assoc();
+        if (password_verify($password, $user['password'])) {
+            $_SESSION['name'] = $user['name'];
+            $_SESSION['email'] = $user['email'];
+
+            if ($user['role'] === 'admin') {
+                header("Location: admin_page.php");
+            } else {
+                header("Location: user_page.php");
             }
+            exit();
         }
     }
-
     $_SESSION['login_error'] = 'Incorrect email';
     $_SESSION['active_form'] = 'login';
     header("location: index.php");
